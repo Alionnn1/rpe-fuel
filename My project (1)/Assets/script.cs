c430d414b6309f891451c1c2a6089629767a5588
Alionnn1 <zixdsameh@gmail.com.>
2025-10-20 05:27:17 -0700

    lab3

--- My project (1)/Assets/script.cs
+++ My project (1)/Assets/script.cs
@@ -13,10 +13,11 @@ public Transform groundCheck;
 public float  groundCheckRadius;
 public LayerMask whatIsGround;
 private bool grounded;
-
+private Animator anim;
 
     void Start()
     {
+    anim = GetComponent<Animator>();
         
     }
 
@@ -46,6 +47,10 @@ private bool grounded;
             GetComponent<SpriteRenderer>().flipX = false;
         }
     }
+
+    anim.SetFloat("speed",Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+    anim.SetFloat("Height", GetComponent<Rigidbody2D>().velocity.y);
+    anim.SetBool("Grounded", grounded);
  }
     void Jump()
     {
